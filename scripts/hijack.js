452bd4091047f77038d216e6902a46e548c94da6
LUAN\luankhong <lkdrumandbass@csu.fullerton.edu>
2017-03-01 02:10:30 -0800

    upload files

---
+++ scripts/hijack.js
@@ -0,0 +1,11 @@
+//http://stackoverflow.com/questions/3871358/get-all-the-href-attributes-of-a-web-site
+var links = document.getElementsByTagName('a');
+
+var linksArray = [].slice.call(links);
+
+function addHandler(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+    });}
+
+linksArray.forEach(addHandler);
---
+++ scripts/main.js
@@ -0,0 +1,98 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
+
+function setDetails(imageUrl, titleText) {
+    'use strict';
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict';
+    thumb.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumb);
+        showDetails() ;
+    });
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailArray = [].slice.call(thumbnails);
+    return thumbnailArray;
+}
+
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    'use strict';
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function() {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+//*****************assignment 4 specific**********************************
+function addThumbKeyPressDetails(key) {
+    'use strict';
+    var thumbArray= getThumbnailsArray();
+    setDetailsFromThumb(thumbArray[key % 49]);
+    showDetails() ;
+}
+//*****************assignment 4 specific**********************************
+
+function addKeyPressHandler(thumbnailsLength) {
+    'use strict';
+    document.body.addEventListener('keyup', function (event)
+    {
+        event.preventDefault();
+        console.log(event.keyCode);
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+        for (var  x = 49; x < 49 + thumbnailsLength && x <= 57; x++) { //Make sure x is within array range AND less than 57.
+            if (event.keyCode === x) {
+                addThumbKeyPressDetails(x);
+            }
+        }
+    });
+}
+
+function initializeEvents() {
+    'use strict';
+    var thumbnails = getThumbnailsArray();
+    var thumbnailsLength = thumbnails.length;
+    thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler(thumbnailsLength);
+}
+
+initializeEvents();
